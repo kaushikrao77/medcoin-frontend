4f9044781ebcb9b517f3e8848fc2ac0b2f0999ad
kaushikrao77 <kaushikrao77@gmail.com>
2022-05-20 12:45:21 +0530

    add auth pages

---
+++ pages/sgin-in.js
@@ -0,0 +1,9 @@
+import styles from "../styles/auth.module.css";
+
+export default function SignIn() {
+  return (
+    <div className={styles.SignIn}>
+      <div></div>
+    </div>
+  );
+}
---
+++ pages/sign-up.js
@@ -0,0 +1,9 @@
+import styles from "../styles/auth.module.css";
+
+export default function SignUp() {
+  return (
+    <div className={styles.SignUp}>
+      <div></div>
+    </div>
+  );
+}
