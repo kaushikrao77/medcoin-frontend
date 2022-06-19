05e789cf0772ec03bee69691a66c985766ce62dc
mecodeslol <prashanthsangram17@gmail.com>
2022-06-19 12:04:55 +0530

    added landing page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { Typography, Button } from "@mui/material";
+import Link from "next/link";
+
+
 import Navbar, {
   HospitalNavbar,
   ResearchNavbar,
@@ -11,6 +15,7 @@ import SignIn from "./sign-in";
 import SignUp from "./sign-up";
 import { Homepage } from "../components/Homepage";
 import { useEffect, useState } from "react";
+
 
 export default function Home() {
   const [role, setRole] = useState(0);
@@ -24,6 +29,8 @@ export default function Home() {
       setRole(1);
     }
   }, []);
+
+  
   return (
     <div className={styles.Home}>
       <Head>
@@ -38,8 +45,67 @@ export default function Home() {
       ) : (
         <LandingNavbar />
       )}
-      {/* <Navbar /> */}
-      <main className={styles.main}>{/* <SignIn /> */}</main>
+
+    <div className={styles.text}>
+      <Typography variant="h3" gutterBottom sx={{ marginBottom:"30px",fontWeight:"700"}}>
+          Facilitating Secure Medical Data Aggregation.
+      </Typography>
+      <Typography variant="h5" className={styles.subHeading} sx={{ marginBottom:"30px"}}>
+          We help you manage, store and view your medical records in a secure and efficient way.
+      </Typography>
+      <div className={styles.buttons}>
+      
+      <Link href="/sign-up" className={styles.button}>
+        <Button
+            variant="contained"
+            type="submit"
+            sx={{ marginTop: "10px", marginRight:"30px", minWidth:"200px",minHeight:"50px"}}
+          >
+            Sign Up
+        </Button>
+      </Link>
+
+      <Link href="/sign-in">
+          <Button
+              variant="outlined"
+              type="submit"
+              sx={{ marginTop: "10px",minWidth:"200px",minHeight:"50px" }}
+              >
+                Sign In
+          </Button>
+      </Link>      
+    </div>
+    </div>
+    
+      
+    
+    
     </div>
   );
 }
+
+
+
+
+
+
+
+{/* <div className={styles.signUp}>
+            
+          </div>
+
+
+          <div className={styles.signIn}>
+            <Typography
+              sx={{
+                  fontSize: 24,
+                  textAlign: "center",
+                  marginTop: "5px",
+                }}
+                  >
+                  Already have an account?
+            </Typography>
+        
+            
+          
+          </div> */}
